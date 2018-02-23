32e7a46433a62690d3ba86bfb83cafa4f9767668
cwouyang <cwouyang@protonmail.com>
2018-02-23 11:33:09 +0800

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("Async File Downloader")
+        .version("0.1")
+        .about("Given an URL which responses a JSON array containing a list of target URLs. This \
+            program downloads files from the target URLs.")
+        .author("cwouyang <cwouyang@protonmail.com>")
+        .args_from_usage("<JSON URL> 'JSON array contains a list of target URL to download'")
+        .get_matches();
 }
